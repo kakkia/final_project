fbeb1386ed6469a5bea7ae90c2908491936a71b1
kakkia <98701415+kakkia@users.noreply.github.com>
2022-04-05 10:48:30 +0200

    Add files via upload
    First test for Streamlit app

---
+++ streamlit_test.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Talking Hands")
+
+st.header("The first Italian Gestures Classification app")
+
+from PIL import Image
+image = Image.open('Desktop/WBS_bootcamp/final_project/img_streamlit/gestures.jpg')
+
+st.image(image, use_column_width=True)
+
+st.write("""
+When speaking Italian it is mandatory that you express your emotions with your hands. This app helps you to use the appropriate gesture when you just can't find the words.\n
+So far I have trained a model to predict three key expressions: "what?", "shoo", and "excellent".
+How about giving us a hand to test it?""")
+
+uploaded_file = st.file_uploader("Upload a picture to predict your gesture", type="jpg, png")
+
+
