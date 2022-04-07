f8c2b38220effa8ae17fc1795bc0696c2018f3e7
kakkia <98701415+kakkia@users.noreply.github.com>
2022-04-07 15:59:52 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -9,15 +9,18 @@ def load_image():
 def main():
     st.title("Talking Hands")
     st.header("The first Italian Gestures Classification app")
-    from PIL import Image
-    image = Image.open('img_streamlit/gestures.jpg')
+    
 
     st.image(image, use_column_width=True)
 
     st.write("""
 When speaking Italian it is mandatory that you express your emotions with your hands. My app will help you to use the appropriate gesture when you just can't find the words.\n
-I have been training a neural network model to predict three key expressions: "what?", "shoo", and "excellent".
-#### How about giving us a hand to test it?""")
+So far I have been training a neural network model to predict the following key expressions:""")
+
+    from PIL import Image
+    image = Image.open('img_streamlit/gestures.jpg')
+      
+    st.write("#### Give us a hand!")
     load_image()
 
 if __name__ == '__main__':
