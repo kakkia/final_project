c588fbced06abb04491c83dcf1cb0d5ea114689e
kakkia <98701415+kakkia@users.noreply.github.com>
2022-04-07 16:04:54 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Pick an image to test')
+    uploaded_file = st.file_uploader(label='Give us a hand')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -16,12 +16,12 @@ def main():
 
     st.write("""
 When speaking Italian it is mandatory that you express your emotions with your hands. My app will help you to use the appropriate gesture when you just can't find the words.\n
-So far I have been training a neural network model to predict only three key expressions and you can find out their meaning after uploading your picture.
-#### Give us a hand!""")
+So far I have been training a neural network model to predict only three key expressions and you can find out their meaning after uploading your picture.""")
+    
     load_image()
 
 if __name__ == '__main__':
     main()
 
 if st.button('Predict your gesture'):
-     st.write('Not now, I will upload my model later')
+     st.write('Not just yet. I will upload my model later')
