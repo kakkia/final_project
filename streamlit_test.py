6e0fc213732f5abb6f80207f31cb642ffad4e91b
kakkia <98701415+kakkia@users.noreply.github.com>
2022-04-07 15:30:36 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,19 +1,26 @@
 import streamlit as st
 
-st.title("Talking Hands")
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an image to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
 
-st.header("The first Italian Gestures Classification app")
+def main():
+    st.title("Talking Hands")
+    st.header("The first Italian Gestures Classification app")
+    from PIL import Image
+    image = Image.open('img_streamlit/gestures.jpg')
 
-from PIL import Image
-image = Image.open('img_streamlit/gestures.jpg')
+    st.image(image, use_column_width=True)
 
-st.image(image, use_column_width=True)
+    st.write("""
+When speaking Italian it is mandatory that you express your emotions with your hands. My app will help you to use the appropriate gesture when you just can't find the words.\n
+I have been training a neural network model to predict three key expressions: "what?", "shoo", and "excellent".
+#### How about giving us a hand to test it?""")
+    load_image()
 
-st.write("""
-When speaking Italian it is mandatory that you express your emotions with your hands. This app helps you to use the appropriate gesture when you just can't find the words.\n
-So far I have trained a model to predict three key expressions: "what?", "shoo", and "excellent".
-How about giving us a hand to test it?""")
-
-uploaded_file = st.file_uploader("Upload a picture to predict your gesture", type="jpg, png")
+if __name__ == '__main__':
+    main()
 
 
