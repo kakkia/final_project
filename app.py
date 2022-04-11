406a1d373a541d8c19bab20c517bc71809ec5e8f
kakkia <98701415+kakkia@users.noreply.github.com>
2022-04-11 10:43:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import streamlit as st
 import tensorflow as tf
-import tensorflow.keras.backend as K
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = load_model('https://drive.google.com/file/d/1w-dni_INNcLBhjuJkm-4HxLMjlgfS-XI/view?usp=sharing')
-  model._make_predict_function()
-  model.summary()
-  return model
+    model = tf.keras.models.load_model('Desktop/WBS_bootcamp/final_project/my_model/my_new_model.hdf5')
+    model.summary()
+    return model
 
 def main():
     st.title("Talking Hands")
     st.header("The first Italian Gestures Classification app")
     from PIL import Image
-    image = Image.open('img_streamlit/gestures.jpg')
+    image = Image.open('Desktop/WBS_bootcamp/final_project/img_streamlit/gestures.jpg')
 
     st.image(image, use_column_width=True)
 
@@ -23,41 +25,64 @@ When speaking Italian it is mandatory that you express your emotions with your h
     
 if __name__ == '__main__':
     main()
-    uploaded_file = st.file_uploader(label='Give us a hand and upload your picture')
-    #sentence = st.text_input('Input your sentence here:')
-    model= load_model()
-    if uploaded_file:
-        y_hat = model.predict(uploaded_file)
-
-#if st.button('Predict your gesture'):
-#     st.write('Not just yet. I will upload my model later')
-
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-def import_and_predict(uploaded_file, model):
-  size = (180,180)    
-  image = ImageOps.fit(uploaded_file, size, Image.ANTIALIAS)
-  image = np.asarray(image)
-  img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.   
-  img_reshape = img[np.newaxis,...]
     
-  prediction = model.predict(img_reshape)  
-  return prediction
+uploaded_file = st.file_uploader(label='Give us a hand and upload your picture', type=None)
+predict_hand = st.button('Predict your gesture')
 
-if uploaded_file is None:
-    st.text("Please upload an image")
-else:
+############################################### -- so far so good!
+          
+class_names = ["what", "shoo", "perfect"] 
+all_pred = ["0.3", "0.3", "0.3"]
+
+def predict_class(image):
+    classify_model = tf.keras.models.load_model('Desktop/WBS_bootcamp/final_project/my_model/my_new_model.hdf5')
+    test_image = image.resize((180,180))
+    test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    test_image /= 255.
+    prediction = classify_model.predict(test_image)
+    winner = np.argmax(prediction)
+    all_preds = prediction[0]
+    predicted_class = f"Your hand is predicted as {class_names[winner]}"
+    return predicted_class, all_pred, winner
+
+def hand_description(winner):
+    
+    if winner == 0:
+        text = st.markdown("""<h4>What?</h4>
+        <ul>
+        <li>The tips of the fingers of one hand are brought sharply together to form an upward-pointing cone.</li>
+        <li>The hand can either be held motionless or be shaken more or less violently up and down.</li>
+        <li>How fast you move it, depends on the degree of impatience expressed.</li>
+        <li>Don't be afraid of using it when someone tells you something unexpectedly upsetting.</li>
+        </ul>""", True)
+    elif winner == 1:
+        text = st.markdown("""<h4>Contempt</h4>
+        <ul>
+        <li>The flat hand slowly moves as to follow the people you are addressing.</li>
+        <li></li>
+        <li>In parenting, it can be moved up and down to suggest you will be punished for what you did.</li>
+        <li></li>
+        </ul>""",True)
+    else:
+        text = st.markdown("""<h4>Excellent</h4>
+        <ul>
+        <li>This gesture express both approval and hearty satifaction.</li>
+        <li>It is typical of the good-natured and contented gourmet.</li>
+        <li>Use it anythime you find something delicious, or you completely agree with someone.</li>
+        <li>And it's not just for food.</li>
+        </ul>""",True)
+    return text
+
+if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, use_column_width=True)
-    predictions = import_and_predict(image, model)
-    score = tf.nn.softmax(predictions[0])
-    st.write(prediction)
-    st.write(score)
-    print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+    st.image(image, caption='Your hand', width=None)
+else:
+    st.warning('After uploading your picture click: Predict your gesture')
+
+if predict_hand:   
+    result, all_preds, winner = predict_class(image)
+    st.subheader(result)
+    st.text = hand_description(winner)
+else:
+    st.write('')
